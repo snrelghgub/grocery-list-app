41118593b635b3dc3bb6db0c0ebfdca4acdebc98
snrelgh se <snrelghmail@gmail.com>
2020-09-21 20:00:58 +1000

    add component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Input from './components/Input'
+import List from './components/List'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
-     
+       <Header />
+       <Input />
+       <List />
+       <Footer />
     </div>
   );
 }
