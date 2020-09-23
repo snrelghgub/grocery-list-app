e2a304ec9e5494394f6a6e1dc4962b96f1530cb8
snrelgh se <snrelghmail@gmail.com>
2020-09-23 21:26:09 +1000

    Updates

---
+++ precache-manifest.259ce0b096cdd13585bec080b0983b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0253eca4f597e1548a230980d76cad2a",
+    "url": "/grocery-list-app/index.html"
+  },
+  {
+    "revision": "a89ec6ba6aa614b9d7db",
+    "url": "/grocery-list-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2174729fd517d31bb283",
+    "url": "/grocery-list-app/static/css/main.d750e4a9.chunk.css"
+  },
+  {
+    "revision": "a89ec6ba6aa614b9d7db",
+    "url": "/grocery-list-app/static/js/2.b278d53f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/grocery-list-app/static/js/2.b278d53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2174729fd517d31bb283",
+    "url": "/grocery-list-app/static/js/main.2ed99c3c.chunk.js"
+  },
+  {
+    "revision": "66b57b37d1a392384c26",
+    "url": "/grocery-list-app/static/js/runtime-main.4343eafd.js"
+  }
+]);
